ce174dfca13cfbea22b0789cd23a47d1fcb88207
Ryan Dioneda <er.dioneda@gmail.com>
2026-04-04 13:01:37 -0400

    Cleaned package

--- Runtime/UnityAppleReplayKitApi.cs
+++ Runtime/UnityAppleReplayKitApi.cs
@@ -1,15 +1,15 @@
 using UnityEngine;
 
-namespace Dioeos.UnityAppleReplayKit 
+namespace Dioeos.UnityAppleReplayKit
 {
-  public static class NativeHello 
+  public static class UnityAppleReplayKitApi
   {
     public static string SayHello()
     {
 #if UNITY_IOS && !UNITY_EDITOR
       return NativeHelloBridgeiOS.SayHello();
 #else
-      return "Not properly calling";
+      return "Not Properly calling";
 #endif
     }
   }
