1d411b3368164bbca2d60a1d17bd55e343bed6a0
Ryan Dioneda <er.dioneda@gmail.com>
2026-04-05 21:51:08 -0400

    Added meta files for ReplayKitBridge+Manager

--- Runtime/iOS/ReplayKitManageriOS.cs
+++ Runtime/iOS/ReplayKitManageriOS.cs
@@ -1,32 +1,44 @@
 using System;
-using System.Runtime.InteropServices
+using System.Runtime.InteropServices;
 
 namespace Dioeos.UnityAppleReplayKit
 {
   internal static class ReplayKitManageriOS
   {
     [DllImport("__Internal")]
-    private static extern bool replaykit_is_available(void);
+    private static extern bool replaykit_is_available();
 
     [DllImport("__Internal")]
-    private static extern bool replaykit_start_recording(void);
+    private static extern bool replaykit_start_recording();
 
     [DllImport("__Internal")]
-    private static extern bool replaykit_stop_recording(void);
+    private static extern bool replaykit_stop_recording();
 
     internal static bool IsAvailable()
     {
+#if UNITY_IOS && !UNITY_EDITOR
       return replaykit_is_available();
+#else
+      return false;
+#endif
     }
 
     internal static bool StartRecording()
     {
+#if UNITY_IOS && !UNITY_EDITOR
       return replaykit_start_recording();
+#else
+      return false;
+#endif
     }
 
     internal static bool StopRecording()
     {
+#if UNITY_IOS && !UNITY_EDITOR
       return replaykit_stop_recording();
+#else
+      return false;
+#endif
     }
   }
 }
